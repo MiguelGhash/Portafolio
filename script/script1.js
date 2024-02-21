af22beff968ec22ee2f929c80938b40d22b2b271
Miguel Ghash <Miguel.Rodriguez.1926hotmail.com>
2024-02-21 03:43:21 -0500

    portafolio primer update

---
+++ script/script1.js
@@ -0,0 +1,10 @@
+function toggleSkills(numero) {
+    const desplegable = document.getElementById("skills" + numero);
+    if (desplegable.style.maxHeight === "0px" || !desplegable.style.maxHeight) {
+      // Expandir el div desplegable
+      desplegable.style.maxHeight = desplegable.scrollHeight + "px";
+    } else {
+      // Contraer el div desplegable
+      desplegable.style.maxHeight = "0";
+    }
+  }
